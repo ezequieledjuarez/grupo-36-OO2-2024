7b13e80190a9fe27782b9ee04d4b8b7359be7d6e
ezejuarez <ezequieledjuarez@gmail.com>
2024-06-19 07:01:31 -0300

    Agrego entidad y dto de producto

--- src/main/java/com/unla/grupo36/dtos/ProductDTO.java
+++ src/main/java/com/unla/grupo36/dtos/ProductDTO.java
@@ -12,6 +12,7 @@ public class ProductDTO {
 	private int id;
 	private String name;
 	private String description;
-	private float pruchasePrice;
-	private float sellingPrice;
+	private Float purchasePrice;
+	private Float sellingPrice;
+	private boolean isAvailable;
 }
--- src/main/java/com/unla/grupo36/entities/Product.java
+++ src/main/java/com/unla/grupo36/entities/Product.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,16 +24,23 @@ public class Product {
 	private String name;
 	private String description;
 	@Column(name="purchase_price", unique=false, nullable=false)
-	private float pruchasePrice;
+	private Float purchasePrice;
 	@Column(name="selling_price", unique=false, nullable=false)
-	private float sellingPrice;
+	private Float sellingPrice;
+	private Boolean isAvailable;
 	
-	public Product(String name, String description, float pruchasePrice, float sellingPrice) {
+	@OneToOne
+	@JoinColumn(name = "stock_id", referencedColumnName = "id")
+    private Stock stock;
+	
+	public Product(String name, String description, float purchasePrice, Float sellingPrice, Integer id, Boolean isAvailable) {
 		super();
+		this.id = null == id ? 0 : id;
 		this.name = name;
 		this.description = description;
-		this.pruchasePrice = pruchasePrice;
-		this.sellingPrice = sellingPrice;
+		this.purchasePrice = purchasePrice;
+		this.sellingPrice = null == sellingPrice ? purchasePrice * 1.40f : sellingPrice;
+		this.isAvailable = null == isAvailable ? true: false;
 	}
 	
 	
